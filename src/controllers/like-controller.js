c59b725d0770ef06a928ef2ffd66407cffa9698a
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-03-19 00:04:52 +0530

    added like controller

---
+++ src/controllers/like-controller.js
@@ -0,0 +1,28 @@
+import LikeService from "../services/like-service.js";
+
+const likeService = new LikeService();
+
+export const toggleLike = async (req, res) => {
+  try {
+    const response = await likeService.toggleLike(
+      req.params.modelId,
+      req.params.modelType,
+      req.body.userId
+    );
+
+    return res.status(200).json({
+      success: true,
+      data: response,
+      message: "Successfully toggled like",
+      err: {},
+    });
+  } catch (err) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      data: {},
+      message: "Something went wrong",
+      err: error,
+    });
+  }
+};
