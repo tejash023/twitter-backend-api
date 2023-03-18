0b3df08ac6cfaa8ee4fbde7da938cb969dab9230
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-03-19 00:19:19 +0530

    added sample models for like

--- src/models/like.js
+++ src/models/like.js
@@ -28,6 +28,17 @@ export default Like;
 
 /*
   onModel: Type of Model - could be either tweet or a comment
-  likeable: ref Path for onModel
+  likeable: Tweet id for user will like/unline
   user: user details
+
+  {
+    "_id" : ObjectId("6416070bdb42396efb5e7d78"),
+    "onModel" : "Tweet",
+    "likeable" : ObjectId("6415fae963b49bff5554b15c"),
+    "user" : ObjectId("6414aefb80dbc3da1ff58a01"),
+    "createdAt" : ISODate("2023-03-18T18:46:35.448+0000"),
+    "updatedAt" : ISODate("2023-03-18T18:46:35.448+0000"),
+    "__v" : NumberInt(0)
+  }
+
 */
