04dfe10dd311fadaa5bbaf48ab3b88585423dfdf
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-03-19 00:09:15 +0530

    added v1 routes for toggle likes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 
 import { createTweet } from "../../controllers/tweet-controller.js";
+import { toggleLike } from "../../controllers/like-controller.js";
 
 const router = express.Router();
 
-router.use("/tweets", createTweet);
+router.post("/tweets", createTweet);
+
+router.post("/likes/toggle", toggleLike);
 
 export default router;
